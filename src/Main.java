7c71d1c6500aaa2c29862c1e07c4bce7d4edbddc
Igor Rubanov <fomagreen@yandex.ru>
2023-02-04 20:07:38 +0300

    Add new object

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-
-        int amount = 15_400;
-        int oneMile = 20;
-
-        int miles = amount / oneMile;
-
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
         System.out.println(miles);
-
-
     }
 }
