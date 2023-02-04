7fdabda74e48a3d64825b17786e4df97f66f6643
Igor Rubanov <fomagreen@yandex.ru>
2023-02-04 17:41:56 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int amount = 15_400;
+        int oneMile = 20;
+
+        int miles = amount / oneMile;
+
+        System.out.println(miles);
+
+
+    }
+}
